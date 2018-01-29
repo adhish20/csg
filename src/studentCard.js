100b173ace8a7ed66fc8b75f4fb7223858f92b6c
Adhish Singla <adhisingla94@gmail.com>
2018-01-29 15:20:43 +0530

    Students Listing Page

---
+++ src/studentCard.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+  card: {
+    width: 250,
+    minHeight: 300,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  padding: {
+    paddingTop: 15,
+    paddingLeft: 20,
+    paddingBottom: 5,
+  },
+  media: {
+    height: 240,
+  },
+});
+
+class StudentCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,
+            position: props.position,
+            image: props.image,
+        };
+    }
+
+    state = { expanded: false };
+
+    handleExpandClick = () => {
+        this.setState({ expanded: !this.state.expanded });
+    };
+    render() {
+        return (
+            <div className={this.props.classes.padding}>
+                <Card className={this.props.classes.card}>
+                    <CardMedia
+                      className={this.props.classes.media}
+                      image={this.state.image}
+                      title={this.state.name}
+                    />
+                    <CardContent>
+                      <Typography type="title">
+                        {this.state.name}
+                      </Typography>
+                      <Typography component="p">
+                          {this.state.position}
+                      </Typography>
+                      </CardContent>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(StudentCard);
--- src/students.js
+++ src/students.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+import StudentCard from './studentCard';
+import axios from 'axios';
+
+const styles = theme => ({
+  flex: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+});
 
 class Students extends Component {
-  render() {
-    return (
-        <div>
-            Students
-        </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: []
+        };
+    }
+
+    componentDidMount() {
+      var th = this;
+      this.serverRequest =
+        axios.get("./student.json")
+          .then(function(result) {
+            th.setState({
+              cards: result.data.student
+            });
+        })
+    }
+
+    render() {
+      return (
+          <div className={this.props.classes.flex}>
+          {this.state.cards.map(function(card) {
+            return (
+              <StudentCard key={card.key} name={card.name} image={card.image} position={card.position} />
+            );
+          })}
+          </div>
+      );
+    }
 }
 
-export default Students;
+export default withStyles(styles)(Students);
